5273331890a36e5586698878b262f902033d9a05
Ivan228 <oacorv@gmail.com>
2026-06-23 12:39:07 +0200

    change prisma import

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 // prisma/seed.ts
 import 'dotenv/config';
-import { PrismaClient } from '../app/generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from "@prisma/adapter-pg";
 import { seoBrands, seoCategories, seoWatches, articles } from './seed-data';
 
